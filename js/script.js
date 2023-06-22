89050320c568d7e60492440337eaa0964a65eb1a
TaccLuc <tacc.luca@gmail.com>
2023-06-22 12:59:26 +0200

    Add: comparison function in appended cells

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const difficulty81 = document.getElementById('difficulty_81');
 const difficulty49 = document.getElementById('difficulty_49');
 const resetButton = document.getElementById('resetButton');
 const cellContainer = document.querySelector('.cell_container');
+const bombs = [];
 
+// DIFFICULTY BUTTONS
 let difficulty = 0;
 
 difficulty100.addEventListener('click', function(){
@@ -19,9 +21,13 @@ difficulty49.addEventListener('click', function(){
     difficulty = 49;
 });
 
+// PLAY BUTTON
 let resetCounter = 0;
 playButton.addEventListener('click', function(){
-    
+
+    randomNotRepeatedNumber(16, difficulty, bombs);
+    console.log(bombs);
+
     if (difficulty == 0){
         alert('Seleziona una difficoltà!');
     }
@@ -35,7 +41,7 @@ playButton.addEventListener('click', function(){
     }
 });
 
-
+// RESET BUTTON
 resetButton.addEventListener('click', function(){
     cellContainer.innerHTML = '';
     difficulty = 0;
@@ -52,10 +58,9 @@ function generateTable(x) {
         cell.innerHTML = i;
         cell.addEventListener('click', function(){
             this.classList.toggle('active');
-            console.log(i);
+            comparison(i, bombs, cell)
         });
         cellContainer.append(cell);
-     
     }
 }
 
@@ -63,20 +68,24 @@ function randomNumber (min, max){
    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomNotRepeatedNumber(x){
-    const randomNumbersArray = [];
-    
+function randomNotRepeatedNumber(x, y, randomNumbersArray){
+
     do{
-        const aNumber = randomNumber(1, x);
+        const aNumber = randomNumber(1, y);
     
         if (!randomNumbersArray.includes(aNumber)) {
             randomNumbersArray.push(aNumber);
         }
     }
-
     while (randomNumbersArray.length < x) {
     }
 
     return randomNumbersArray;
+}
+
+function comparison(x, y, singleElement) {
+    if (y.includes(x)) {
+        singleElement.innerHTML = 'PERSO!';
+    }
 }
 
