ff76bca1c6289851040fc523b081c1bb59ba028c
TaccLuc <tacc.luca@gmail.com>
2023-06-22 16:51:28 +0200

    Fix: array generation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ELEMENTS
 const playButton = document.getElementById('playButton');
 const difficulty100 = document.getElementById('difficulty_100');
 const difficulty81 = document.getElementById('difficulty_81');
@@ -5,10 +6,10 @@ const difficulty49 = document.getElementById('difficulty_49');
 const resetButton = document.getElementById('resetButton');
 const cellContainer = document.querySelector('.cell_container');
 const scoreBoard = document.querySelector('.scoreBoard');
-const bombs = [];
 
-// SCORE
+// SCORE + BOMBS
 let score = 0;
+let bombs = [];
 
 // DIFFICULTY BUTTONS
 let difficulty = 0;
@@ -26,23 +27,19 @@ difficulty49.addEventListener('click', function(){
 });
 
 // PLAY BUTTON
-let resetCounter = 0;
 playButton.addEventListener('click', function(){
-
-    randomNotRepeatedNumber(16, difficulty, bombs);
-    console.log(bombs);
 
     if (difficulty == 0){
         alert('Seleziona una difficoltà!');
     }
-    else if (resetCounter == 1){
+    else {
         cellContainer.innerHTML = '';
-        generateTable(difficulty);
+        bombs = [];
+        randomNotRepeatedNumber(16, difficulty, bombs);
+        console.log(bombs);
+        generateTable(difficulty); 
     }
-    else{
-        generateTable(difficulty)
-        resetCounter = 1;
-    }
+
 });
 
 // RESET BUTTON
@@ -50,31 +47,33 @@ resetButton.addEventListener('click', function(){
     cellContainer.innerHTML = '';
     difficulty = 0;
     score = 0;
-    cellContainer.classList.remove('lostBoard');
+    document.getElementById('score').innerHTML = score;
+    cellContainer.classList.remove('endBoard');
 });
 
 // FUNCTIONS
 function generateTable(x) { 
     for (let i = 1; i <= x; i++) {
+
         const cell = document.createElement('div');
         cell.classList.add('cell', 'cell_' + x);
         cell.innerHTML = i;
+        cellContainer.append(cell);
+
         let clicked = false;
-        
         cell.addEventListener('click', function(){
-            comparison(i, bombs, cell)
+            gameLost(i, bombs, cell)
             this.classList.add('active');
             if(score === x - 17 || bombs.includes(i)){
-                cellContainer.classList.add('lostBoard');
+                cellContainer.classList.add('endBoard');
             }
             else if (!clicked && !bombs.includes(i)) {
                 score++;
                 clicked = true;
                 document.getElementById('score').innerHTML = score;
             }
-
         });
-        cellContainer.append(cell);
+        
     }
 }
 
@@ -92,11 +91,9 @@ function randomNotRepeatedNumber(x, y, randomNumbersArray){
     }
     while (randomNumbersArray.length < x) {
     }
-
-    return randomNumbersArray;
 }
 
-function comparison(x, y, singleElement) {
+function gameLost(x, y, singleElement) {
     if (y.includes(x)) {
         singleElement.classList.add('lost');
         singleElement.innerHTML = 'HAI PERSO!';
